feac5c33c723cf9ea3145f683532c010c5e287f4
Didier Domínguez <dadu0699@hotmail.com>
2022-02-28 23:34:13 -0600

    [ADD] Facts

--- app.pl
+++ app.pl
@@ -1,5 +1,432 @@
-mensaje:- nl,
-    write('Ejemplo: "El hola mundo de Prolog" cargado.'),
-    nl,
-    nl.
-saludo:- write('Hello World!'), mensaje.
+% Records with uppercase letters were converted to lowercase
+% español is change to espanol
+
+% Customer list
+customer(1, jose, morales, guatemala, 27, soltero).
+customer(2, fabrizio, sartini, italia, 25, casado).
+customer(3, gustavo, molina, holanda, 15, soltero).
+customer(4, maria, gutierrez, guatemala, 20, soltero).
+customer(5, cornelio, bustamante, guatemala, 18, soltero).
+customer(6, loraine, cortez, espanol, 34, divorciado). % española is change to español
+customer(7, paco, estanley, guatemala, 30, soltero).
+customer(8, daniela, samayoa, guatemala, 21, casado).
+customer(9, silvia, santis, el_salvador, 50, casado).
+customer(10, luis, ortega, el_salvador, 35, soltero).
+customer(11, tatiana, lopez, el_salvador, 28, soltero).
+customer(12, ronny, paiz, honduras, 20, soltero).
+customer(13, cesar, ortiz, guatemala, 36, soltero).
+customer(14, margot, claus, usa, 32, soltero).
+customer(15, jorge, calderon, venezuela, 26, casado).
+customer(17, maira, martinez, mexico, 25, soltero).
+customer(18, jazmin, dolores, venezuela, 27, casado).
+customer(19, mauro, hernandez, espanol, 22, soltero).
+customer(20, luis, sagastume, costa_rica, 20, soltero).
+customer(21, rcharson, patric, inglaterra, 30, casado).
+customer(22, montheri, lois, inglaterra, 32, casado).
+customer(23, silverio, perez, mexico, 33, casado).
+customer(24, monica, suarez, mexico, 30, casado).
+
+% Department list
+department(1, peten, 8, espanol, tropical).
+department(2, izabal, 5, espanol, calor).
+department(3, alta_verapaz, 6, espanol, frio).
+department(4, quiche, 5, katchikel, frio).
+department(5, huehuetenango, 7, espanol, frio).
+department(6, escuintla, 2, espanol, calor).
+department(7, san_marcos, 3, ingles, calor).
+department(8, jutiapa, 4, katchikel, calor).
+department(9, baja_verapaz, 5, ingles, templado).
+department(10, santa_rosa, 4, ingles, calor).
+department(11, zacapa, 3, ingles, calor).
+department(12, suchitepequez, 5, espanol, templado).
+department(13, chiquimula, 4, katchikel, calor).
+department(14, guatemala, 0, ingles, templado).
+department(15, jalapa, 4, ingles, calor).
+department(16, chimaltenango, 3, katchikel, calor).
+department(17, quetzaltenango, 3, ingles, frio).
+department(18, el_progreso, 4, katchikel, calor).
+department(19, retalhuleu, 4, ingles, calor).
+department(20, solola, 6, ketchi, frio).
+department(21, totonicapan, 5, ingles, templado).
+department(22, sacatepequez, 2, espanol, templado).
+
+% Hotel list
+hotel(1, camino_real, zona9, 4, 100, 200, 50, 1).
+hotel(2, holiday_inn, zona10, 4, 200, 350, 75, 2).
+hotel(3, hu-nal_ye, km260, 1, 50, 100, 60, 2).
+hotel(4, onetwo, km288, 3, 200, 350, 80, 4).
+hotel(5, paradise, km40, 2, 150, 250, 80, 3).
+hotel(6, tikal, km350, 1, 200, 350, 100, 8).
+hotel(7, atanacio, km102, 5, 350, 700, 100, 12).
+hotel(8, pana_inn, km80, 4, 250, 500, 50, 16).
+hotel(9, tzul, km156, 3, 150, 400, 50, 20).
+hotel(10, ktuz, km150, 3, 300, 600, 50, 18).
+hotel(11, patulul, km67, 3, 300, 500, 150, 7).
+hotel(12, patzul, km90, 2, 250, 400, 100, 17).
+hotel(13, libfresh, km100, 2, 250, 400, 50, 7).
+hotel(14, kenel, km180, 3, 250, 400, 70, 8).
+hotel(15, manoi, km40, 1, 250, 400, 40, 10).
+hotel(16, palmeras, km7, 2, 250, 400, 30, 15).
+hotel(17, ovni, km8, 4, 250, 400, 70, 19).
+hotel(18, ovni2, km270, 5, 250, 400, 95, 21).
+hotel(19, runners, km187, 3, 200, 350, 35, 22).
+hotel(20, tutuc, km365, 4, 150, 280, 25, 14).
+hotel(21, las_palmas, km650, 3, 450, 700, 150, 11).
+hotel(22, kinal, km354, 4, 500, 800, 200, 10).
+hotel(23, prank, km269, 5, 400, 700, 150, 13).
+hotel(24, dona_rosa, km34, 4, 250, 500, 100, 11).
+hotel(25, los_pulpos, km70, 3, 500, 700, 100, 6).
+hotel(26, las_canarias, km65, 2, 450, 600, 200, 10).
+hotel(27, hawaii, km123, 1, 400, 700, 150, 12).
+hotel(28, yocute, km500, 4, 350, 500, 100, 9).
+hotel(29, renuevate, km90, 3, 280, 600, 65, 20).
+hotel(30, las_islas, km241, 2, 340, 600, 80, 14).
+hotel(31, usac, zona12, 3, 100, 50, 7, 4).
+
+% Register list
+registration(20, 1, 3, 5/2/2022, 1, 4).
+registration(27, 5, 6, 5/2/2022, 2, 4).
+registration(10, 13, 18, 29/04/2022, 1, 2).
+registration(12, 10, 30, 29/04/2022, 2, 1).
+registration(11, 5, 16, 26/04/2022, 2, 2).
+registration(24, 2, 2, 23/2/2022, 2, 3).
+registration(9, 11, 8, 23/04/2022, 1, 3).
+registration(8, 15, 25, 23/04/2022, 1, 1).
+registration(7, 10, 15, 22/04/2022, 2, 10).
+registration(19, 10, 30, 2/2/2022, 2, 6).
+registration(23, 1, 1, 2/1/2022, 1, 7).
+registration(22, 24, 10, 2/1/2022, 5, 10).
+registration(6, 8, 20, 17/04/2022, 1, 9).
+registration(41, 20, 2, 15/04/2022, 1, 1).
+registration(45, 24, 3, 15/04/2022, 3, 5).
+registration(5, 2, 12, 15/04/2022, 3, 8).
+registration(43, 22, 15, 15/04/2022, 3, 10).
+registration(42, 21, 18, 15/04/2022, 2, 5).
+registration(32, 10, 22, 15/04/2022, 1, 7).
+registration(39, 18, 22, 15/04/2022, 2, 9).
+registration(38, 17, 23, 15/04/2022, 1, 8).
+registration(37, 15, 24, 15/04/2022, 2, 7).
+registration(30, 8, 3, 12/04/2022, 2, 6).
+registration(28, 6, 4, 12/04/2022, 5, 1).
+registration(4, 3, 5, 12/04/2022, 1, 7).
+registration(29, 7, 10, 12/04/2022, 1, 5).
+registration(31, 9, 20, 12/04/2022, 1, 8).
+registration(33, 11, 30, 12/04/2022, 3, 1).
+registration(34, 12, 31, 12/04/2022, 4, 10).
+registration(26, 4, 4, 10/2/2022, 1, 9).
+registration(3, 7, 10, 10/04/2022, 1, 4).
+registration(25, 3, 2, 1/2/2022, 3, 8).
+registration(21, 3, 5, 1/1/2022, 2, 8).
+registration(2, 4, 10, 08/04/2022, 2, 6).
+registration(44, 23, 6, 07/05/2022, 1, 10).
+registration(40, 19, 21, 07/05/2022, 3, 5).
+registration(35, 13, 24, 07/05/2022, 4, 5).
+registration(36, 14, 26, 07/05/2022, 5, 6).
+registration(18, 10, 31, 07/05/2022, 3, 5).
+registration(15, 14, 7, 04/05/2022, 1, 2).
+registration(1, 1, 2, 04/04/2022, 2, 5).
+registration(14, 12, 30, 02/05/2022, 2, 6).
+registration(13, 9, 28, 01/05/2022, 1, 5).
+
+% Employee list
+employee(1, jose_hernandez, chef, 1).
+employee(2, maria_loarca, mesera, 1).
+employee(3, julio_ortega, administrador, 1).
+employee(4, jorge_po, recepcionista, 1).
+employee(5, paul_coc, oficinista, 1).
+employee(6, enrique, oficinista, 1).
+employee(7, lorena, oficinista, 1).
+employee(8, mario, oficinista, 1).
+employee(9, pablo, limpieza, 1).
+employee(10, yoli, cocinera, 1).
+employee(11, marionelo, administrador, 2).
+employee(12, house, cocinera, 2).
+employee(13, paco, recepcionista, 2).
+employee(14, francisco, mesero, 2).
+employee(15, gustavo, oficinista, 2).
+employee(16, paul, oficinista, 2).
+employee(17, jorge, oficinista, 2).
+employee(18, tatiana, oficinista, 2).
+employee(19, raul, oficinista, 2).
+employee(20, laura, ofinista, 2).
+employee(21, carlos, administrador, 3).
+employee(22, luis, limpieza, 3).
+employee(23, mario, oficinista, 3).
+employee(24, alejandro, oficinista, 3).
+employee(25, edwin, oficinista, 3).
+employee(26, esvin, ofinista, 3).
+employee(27, quelvin, encargado, 3).
+employee(28, mariela, oficinista, 3).
+employee(29, mariana, oficinista, 3).
+employee(30, holer, oficinista, 3).
+employee(31, jose, administrador, 4).
+employee(32, poncio, oficinista, 4).
+employee(33, gerber, oficinista, 4).
+employee(34, rolando, oficinista, 4).
+employee(35, keila, oficinista, 4).
+employee(36, keithlyn, oficinista, 4).
+employee(37, william, oficinista, 4).
+employee(38, iliana, oficinista, 4).
+employee(39, manuel, oficinista, 4).
+employee(40, byron, oficinista, 4).
+employee(41, victor, administrador, 5).
+employee(42, carolina, oficinista, 5).
+employee(43, karen, oficinista, 5).
+employee(44, xara, oficinista, 5).
+employee(45, zoila, oficinista, 5).
+employee(46, francisco, oficinista, 5).
+employee(47, romario, oficinista, 5).
+employee(48, olga, oficinista, 5).
+employee(49, saulo, oficinista, 5).
+employee(50, norlin, oficinista, 5).
+employee(51, yulisa, propietaria, 6).
+employee(52, luis, auxiliar, 6).
+employee(53, pedro, limpieza, 6).
+employee(54, hitler, auxiliar, 6).
+employee(55, mario, auxiliar, 6).
+employee(56, victoria, oficinista, 6).
+employee(57, ugolino, encargado, 6).
+employee(58, julio, auxiliar, 6).
+employee(59, tavo, auxiliar, 6).
+employee(60, britani, auxiliar, 6).
+employee(61, carlos, administrador, 7).
+employee(62, olga, auxiliar, 7).
+employee(63, sapon, auxiliar, 7).
+employee(64, erlin, auxiliar, 7).
+employee(65, woly, auxiliar, 7).
+employee(66, hilario, auxiliar, 7).
+employee(67, manuela, auxiliar, 7).
+employee(68, quax, auxiliar, 7).
+employee(69, claudia, auxiliar, 7).
+employee(70, ramiro, auxiliar, 7).
+employee(71, isaac, propietario, 8).
+employee(72, yesica, auxiliar, 8).
+employee(73, kelia, auxiliar, 8).
+employee(74, rox, auxiliar, 8).
+employee(75, marling, auxiliar, 8).
+employee(76, maggy, limpieza, 8).
+employee(77, canahui, auxiliar, 8).
+employee(78, monica, auxiliar, 8).
+employee(79, jophef, auxiliar, 8).
+employee(80, toti, auxiliar, 8).
+employee(81, lara, administrador, 9).
+employee(82, xica, auxiliar, 9).
+employee(83, noly, auxiliar, 9).
+employee(84, jorge, auxiliar, 9).
+employee(85, beatriz, auxiliar, 9).
+employee(86, raul, auxiliar, 9).
+employee(87, picoro, auxiliar, 9).
+employee(88, gertrudis, auxiliar, 9).
+employee(89, ulugrun, auxiliar, 9).
+employee(90, brian, auxiliar, 9).
+employee(91, wilson, administrador, 10).
+employee(92, zika, auxiliar, 10).
+employee(93, alejandro, auxiliar, 10).
+employee(94, alex, auxiliar, 10).
+employee(95, debora, auxiliar, 10).
+employee(96, marcos, auxiliar, 10).
+employee(97, ilsa, auxiliar, 10).
+employee(98, olga, auxiliar, 10).
+employee(99, jose_auxiliar, auxiliar, 10).
+employee(100, frederic, auxiliar, 10).
+employee(101, paola, propietario, 11).
+employee(102, hugo, auxiliar, 11).
+employee(103, leonel, auxiliar, 11).
+employee(104, victoria, auxiliar, 11).
+employee(105, gabriel, auxiliar, 11).
+employee(106, keila, auxiliar, 11).
+employee(107, carlos, auxiliar, 11).
+employee(108, tampico, auxiliar, 11).
+employee(109, pain, auxiliar, 11).
+employee(110, laura, auxiliar, 11).
+employee(111, daniela, propietario, 12).
+employee(112, gerson, auxiliar, 12).
+employee(113, wendy, auxiliar, 12).
+employee(114, danny, auxiliar, 12).
+employee(115, horacio, auxiliar, 12).
+employee(116, mario, auxiliar, 12).
+employee(117, orfa, auxiliar, 12).
+employee(118, julio, auxiliar, 12).
+employee(119, mario, auxiliar, 12).
+employee(120, francisco, auxiliar, 12).
+employee(121, gustavo, encargado, 13).
+employee(122, dany, auxiliar, 13).
+employee(123, byron, auxiliar, 13).
+employee(124, tati, auxiliar, 13).
+employee(125, gilma, auxiliar, 13).
+employee(126, yessi, auxiliar, 13).
+employee(127, maria, auxiliar, 13).
+employee(128, jose, auxiliar, 13).
+employee(129, jorge, auxiliar, 13).
+employee(130, broilyn, auxiliar, 13).
+employee(131, julio, encargado, 14).
+employee(132, gary, auxiliar, 14).
+employee(133, grouth, auxiliar, 14).
+employee(134, keiht, auxiliar, 14).
+employee(135, bross, auxiliar, 14).
+employee(136, noty, auxiliar, 14).
+employee(137, yorlin, auxiliar, 14).
+employee(138, maryory, auxiliar, 14).
+employee(139, astrid, auxiliar, 14).
+employee(140, maryury, auxiliar, 14).
+employee(141, enrique, oficinista, 15).
+employee(142, lorena, oficinista, 15).
+employee(143, mario, oficinista, 15).
+employee(144, pablo, limpieza, 15).
+employee(145, yoli, cocinera, 15).
+employee(146, mario, administrador, 15).
+employee(147, house, cocinera, 15).
+employee(148, paco, recepcionista, 15).
+employee(149, francisco, mesero, 15).
+employee(150, gustavo, oficinista, 15).
+employee(151, paul, oficinista, 16).
+employee(152, jorge, oficinista, 16).
+employee(153, tatiana, oficinista, 16).
+employee(154, raul, oficinista, 16).
+employee(155, laura, ofinista, 16).
+employee(156, carlos, administrador, 16).
+employee(157, luis, limpieza, 16).
+employee(158, mario, oficinista, 16).
+employee(159, alejandro, oficinista, 16).
+employee(160, edwin, oficinista, 16).
+employee(161, esvin, ofinista, 17).
+employee(162, quelvin, encargado, 17).
+employee(163, mariela, oficinista, 17).
+employee(164, mariana, oficinista, 17).
+employee(165, holer, oficinista, 17).
+employee(166, jose, administrador, 17).
+employee(167, poncio, oficinista, 17).
+employee(168, gerber, oficinista, 17).
+employee(169, rolando, oficinista, 17).
+employee(170, keila, oficinista, 17).
+employee(171, keithlyn, oficinista, 18).
+employee(172, william, oficinista, 18).
+employee(173, iliana, oficinista, 18).
+employee(174, manuel, oficinista, 18).
+employee(175, byron, oficinista, 18).
+employee(176, victor, administrador, 18).
+employee(177, carolina, oficinista, 18).
+employee(178, karen, oficinista, 18).
+employee(179, xara, oficinista, 18).
+employee(180, zoila, oficinista, 18).
+employee(181, francisco, oficinista, 19).
+employee(182, romario, oficinista, 19).
+employee(183, olga, oficinista, 19).
+employee(184, saulo, oficinista, 19).
+employee(185, norlin, oficinista, 19).
+employee(186, yulisa, propietaria, 19).
+employee(187, luis, auxiliar, 19).
+employee(188, pedro, limpieza, 19).
+employee(189, hitler, auxiliar, 19).
+employee(190, mario, auxiliar, 19).
+employee(191, victoria, oficinista, 20).
+employee(192, ugolino, encargado, 20).
+employee(193, julio, auxiliar, 20).
+employee(194, tavo, auxiliar, 20).
+employee(195, britani, auxiliar, 20).
+employee(196, carlos, administrador, 20).
+employee(197, olga, auxiliar, 20).
+employee(198, sapon, auxiliar, 20).
+employee(199, erlin, auxiliar, 20).
+employee(200, woly, auxiliar, 20).
+employee(201, hilario, auxiliar, 21).
+employee(202, manuela, auxiliar, 21).
+employee(203, quax, auxiliar, 21).
+employee(204, claudia, auxiliar, 21).
+employee(205, ramiro, auxiliar, 21).
+employee(206, isaac, propietario, 21).
+employee(207, yesica, auxiliar, 21).
+employee(208, kelia, auxiliar, 21).
+employee(209, rox, auxiliar, 21).
+employee(210, marling, auxiliar, 21).
+employee(211, maggy, limpieza, 22).
+employee(212, canahui, auxiliar, 22).
+employee(213, monica, auxiliar, 22).
+employee(214, jophef, auxiliar, 22).
+employee(215, toti, auxiliar, 22).
+employee(216, lara, administrador, 22).
+employee(217, xica, auxiliar, 22).
+employee(218, noly, auxiliar, 22).
+employee(219, jorge, auxiliar, 22).
+employee(220, beatriz, auxiliar, 22).
+employee(221, raul, auxiliar, 23).
+employee(222, picoro, auxiliar, 23).
+employee(223, gertrudis, auxiliar, 23).
+employee(224, ulugrun, auxiliar, 23).
+employee(225, brian, auxiliar, 23).
+employee(226, wilson, administrador, 23).
+employee(227, zika, auxiliar, 23).
+employee(228, alejandro, auxiliar, 23).
+employee(229, alex, auxiliar, 23).
+employee(230, debora, auxiliar, 23).
+employee(231, marcos, auxiliar, 24).
+employee(232, ilsa, auxiliar, 24).
+employee(233, olga, auxiliar, 24).
+employee(234, jose_auxiliar, auxiliar, 24).
+employee(235, frederic, auxiliar, 24).
+employee(236, paola, propietario, 24).
+employee(237, hugo, auxiliar, 24).
+employee(238, leonel, auxiliar, 24).
+employee(239, victoria, auxiliar, 24).
+employee(240, gabriel, auxiliar, 24).
+employee(241, keila, auxiliar, 25).
+employee(242, carlos, auxiliar, 25).
+employee(243, tampico, auxiliar, 25).
+employee(244, pain, auxiliar, 25).
+employee(245, laura, auxiliar, 25).
+employee(246, daniela, propietario, 25).
+employee(247, gerson, auxiliar, 25).
+employee(248, wendy, auxiliar, 25).
+employee(249, danny, auxiliar, 25).
+employee(250, horacio, auxiliar, 25).
+employee(251, mario, auxiliar, 26).
+employee(252, orfa, auxiliar, 26).
+employee(253, julio, auxiliar, 26).
+employee(254, mario, auxiliar, 26).
+employee(255, francisco, auxiliar, 26).
+employee(256, gustavo, encargado, 26).
+employee(257, dany, auxiliar, 26).
+employee(258, byron, auxiliar, 26).
+employee(259, tati, auxiliar, 26).
+employee(260, gilma, auxiliar, 26).
+employee(261, yessi, auxiliar, 27).
+employee(262, maria, auxiliar, 27).
+employee(263, jose, auxiliar, 27).
+employee(264, jorge, auxiliar, 27).
+employee(265, broilyn, auxiliar, 27).
+employee(266, julio, encargado, 27).
+employee(267, gary, auxiliar, 27).
+employee(268, grouth, auxiliar, 27).
+employee(269, keiht, auxiliar, 27).
+employee(270, bross, auxiliar, 27).
+employee(271, noty, auxiliar, 28).
+employee(272, yorlin, auxiliar, 28).
+employee(273, maryory, auxiliar, 28).
+employee(274, astrid, auxiliar, 28).
+employee(275, maryury, auxiliar, 28).
+employee(276, edwin, oficinista, 28).
+employee(277, esvin, ofinista, 28).
+employee(278, quelvin, encargado, 28).
+employee(279, mariela, oficinista, 28).
+employee(280, julio, oficinista, 28).
+employee(281, jorge, oficinista, 29).
+employee(282, gabriela , agustin, 29).
+employee(283, debora, auxiliar, 29).
+employee(284, marcos, auxiliar, 29).
+employee(285, ilsa, auxiliar, 29).
+employee(286, olga, auxiliar, 29).
+employee(287, jose, auxiliar, 29).
+employee(288, paola, auxiliar, 29).
+employee(289, mario, oficinista, 29).
+employee(290, alejandro, oficinista, 29).
+employee(291, edwin, oficinista, 30).
+employee(292, esvin, ofinista, 30).
+employee(293, quelvin, encargado, 30).
+employee(294, mariela, oficinista, 30).
+employee(295, mariana, oficinista, 30).
+employee(296, mike, oficinista, 30).
+employee(297, nineth, oficinista, 30).
+employee(298, aracely, oficinista, 30).
+employee(299, carmelo, oficinista, 30).
+employee(300, lorein, oficinista, 31).
+employee(301, yulisa, encargado, 31).
